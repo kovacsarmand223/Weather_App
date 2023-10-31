94f0a4f020280e725d29186d51ff6f4e9cd90a73
Kovacs Armand <h160227@stud.u-szeged.hu>
2023-10-31 23:52:47 +0200

    JavaSript API data transform

--- public/JS/weather.js
+++ public/JS/weather.js
@@ -1,11 +1,48 @@
 /*
     https://api.open-meteo.com/v1/forecast?
-    latitude=43.4716&longitude=5.9549&current=temperature_2m,
+    latitude=40.069099&longitude=45.038189&current=temperature_2m,
     is_day,precipitation,windspeed_10m&hourly=temperature_2m,
     relativehumidity_2m,precipitation_probability,windspeed_10m,
     uv_index&daily=weathercode,uv_index_max&timezone=auto
+
+    https://api.open-meteo.com/v1/forecast?
+    latitude=52.52&longitude=13.41&current=temperature_2m,
+    relativehumidity_2m,is_day,precipitation,weathercode,
+    windspeed_10m&hourly=temperature_2m,relativehumidity_2m,
+    precipitation_probability,weathercode,windspeed_10m,
+    uv_index&daily=weathercode,temperature_2m_max,uv_index_max&timezone=auto
 */
 
-export function getWeather(lat, lon, timezone){
+const asd = 0;
+const searchElement = document.querySelector('[data-search]')
+const searchBox = new google.maps.places.SearchBox(searchElement)
+searchBox.addListener('places_changed', () =>{
+    const place = searchBox.getPlaces()[0];
+    if (place == null) return;
+    const latitude = place.geometry.location.lat()
+    const longitude = place.geometry.location.lng()
+    //reacting with server.js app.post(...)
+    fetch('/weather', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json'
+        },
+        body:
+            JSON.stringify({
+            latitude: latitude,
+            longitude: longitude,
+        })
+    }).then(res => res.json()).then(data => {
+        console.log(data)
+        //setWeatherData(data, place.formatted_address)
+    })
+})
+
+function renderWeather(){
+    
+}
+
+function setWeatherData(data, place){
 
 }
--- server.js
+++ server.js
@@ -1,15 +1,76 @@
+/*
+    https://api.open-meteo.com/v1/forecast?
+    latitude=40.069099&longitude=45.038189&current=temperature_2m,
+    is_day,precipitation,windspeed_10m&hourly=temperature_2m,
+    relativehumidity_2m,precipitation_probability,windspeed_10m,
+    uv_index&daily=weathercode,uv_index_max&timezone=auto
+*/
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/weather', (req, res) => {
-  // Your POST request handling logic goes here
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${req.body.latitude}&longitude=${req.body.longitude}&current=temperature_2m,relativehumidity_2m,is_day,precipitation,weathercode,windspeed_10m&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode,windspeed_10m,uv_index&daily=weathercode,temperature_2m_max,uv_index_max&timezone=auto`;
+  axios({
+    url: url,
+    responseType: 'json'
+  }).then(({ data }) => {
+    const response = {
+      current: parseCurrentWeather(data),
+      daily: parseDailyWeather(data),
+      hourly: parseHourlyWeather(data)
+    };
+    
+    res.json(response); // Send the JSON response to the webpage.
+  }).catch(error => {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while fetching weather data.' });
+  });
 });
 
+function parseCurrentWeather({current}){
+  const {is_day: isday, temperature_2m: curr_temp, windspeed_10m: wind_speed, weathercode: icon_code, relativehumidity_2m: rel_hum, precipitation: prob} = current
+  return {
+    isday,
+    curr_temp,
+    prob,
+    icon_code,
+    wind_speed,
+    rel_hum
+  }
+}
+
+function parseDailyWeather({daily}){
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time,
+      icon_code: daily.weathercode[index],
+      temp_2m_max: daily.temperature_2m_max[index],
+      uv_idx: daily.uv_index_max[index]
+    }
+  })
+}
+
+//current needed because the time has to be calculated from now
+function parseHourlyWeather({hourly, current}){
+  return hourly.time.map((time, index) => {
+    return {
+      timestamp: time,
+      icon_code: hourly.weathercode[index],
+      temp_max: hourly.temperature_2m[index],
+      wind_speed: hourly.windspeed_10m[index],
+      rel_him: hourly.relativehumidity_2m[index],
+      prob: hourly.precipitation_probability[index],
+      uv_idx: hourly.uv_index[index]
+    }
+  }).filter(({timestamp}) => timestamp >= current.time)
+}
+
 // Define a route for the root path ('/')
-app.get('/', (request, response) =>{
+app.get('/weather', (request, response) =>{
     response.sendFile("E:/Progs/Webdesign/Weather_App_Project/public/mainPage.html")
 })
 
